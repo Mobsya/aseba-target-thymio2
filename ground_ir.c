34e6850bb0d6c5fe5a2f3825a9a04d822bcb9cdd
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2011-03-11 22:31:37 +0000

    Synchronize ground ir with motor & horizontal prox

--- ground_ir.c
+++ ground_ir.c
@@ -10,23 +10,24 @@ static int state;
 
 void ground_ir_new(unsigned int r, unsigned int l) {	
 	switch(state) {
-		case 0:
+	// +50 to be phase shifted from the horizontal prox & motor
+		case 50:
 			vmVariables.ground_ambiant[0] = r;
 			vmVariables.ground_ambiant[1] = l;
 			PULSE_R = 1;
 			break;
-		case 1:
+		case 53:
 			vmVariables.ground_reflected[0] = r;
 			PULSE_R = 0;
 			PULSE_L = 1;
 			break;
-		case 2: 
+		case 56: 
 			vmVariables.ground_reflected[1] = l;
 			PULSE_L = 0;
 			break;
 	}	
-	
-	if(state++ == 800) 
+	// Period is very important, must match the motor one and horizontal prox
+	if(state++ == 799) 
 		state = 0;	
 }
 
