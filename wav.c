2fac558ed6a7fe197846e5f2e830668daac0763b
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2011-10-26 12:27:56 +0000

    Simplify wav checking

--- wav.c
+++ wav.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "wav.h"
 
 #define RIFF 0x46464952UL
@@ -5,90 +6,6 @@
 #define FMT 0x20746d66UL
 #define DATA 0x61746164UL
 
-static int check_int(FIL * file, unsigned int v) {
-	unsigned int t;
-	unsigned int read;
-	
-	f_read(file, &t, 2, &read);
-	
-	if(read != 2 || t != v)
-		return -1;
-	return 0;
-}
-
-static int check_long(FIL * file, unsigned long v) {
-	unsigned long t;
-	unsigned int read;
-	
-	f_read(file, &t, 4, &read);
-	
-	if(read != 4 || t != v)
-		return -1;
-	return 0;
-}
-
-static int get_long(FIL * file, unsigned long * v) {
-	unsigned int read;
-	
-	f_read(file, v, 4, &read);
-	
-	if(read != 4)
-		return -1;
-	return 0;
-}	
-
-int wav_init(FIL * file) {
-	unsigned long size;
-	unsigned long hz;
-	
-	if(check_long(file, RIFF))
-		return -1;
-	
-	if(get_long(file, &size))
-		return -1;
-	
-	if(check_long(file, WAVE))
-		return -1;
-	
-	if(check_long(file, FMT))
-		return -1;
-	
-	if(check_long(file, 16))
-		return -1;
-	
-	if(check_int(file, 1)) // PCM
-		return -1;
-	
-	if(check_int(file, 1)) // Mono
-		return -1;
-	
-	if(get_long(file, &hz))
-		return -1;
-	
-	if(hz < 7800 || hz > 8000)
-		return -1;
-	
-	if(check_long(file, hz))
-		return -1;
-	
-	if(check_int(file, 1))
-		return -1;
-	
-	if(check_int(file, 8)) // 8 bits
-		return -1;
-	
-	if(check_long(file, DATA))
-		return -1;
-	
-	if(check_long(file, size - 36))
-		return -1;
-		
-	return 0;
-}
-
-void wav_rewind(FIL * file) {
-	f_lseek(file, 44);
-}	
 static const unsigned char wav_header[] = {
 	0x52, 0x49, 0x46, 0x46, // "RIFF"
 	0x00, 0x00, 0x00, 0x00, // Size
@@ -102,6 +19,39 @@ static const unsigned char wav_header[] = {
 	0x64, 0x61, 0x74, 0x61, // "data"
 	0x00, 0x00, 0x00, 0x00  // Size
 };
+
+// Cannot put 44 byte on the stack, it's too much
+static unsigned long __attribute__((far)) read_buffer[11];
+
+int wav_init(FIL * file) {
+	unsigned int read;
+	
+	f_read(file, read_buffer, sizeof(read_buffer), &read);
+	if(read != sizeof(read_buffer)) 
+		return -1;
+	
+	if(read_buffer[1] - 36 != read_buffer[10])
+		return -1;
+	
+	if(read_buffer[6] != read_buffer[7])
+		return -1;
+		
+	if(read_buffer[6] < 7800 || read_buffer[6] > 8000)
+		return -1;
+		
+	read_buffer[6] = read_buffer[7] = 0x1e84;
+	read_buffer[1] = read_buffer[10] = 0;
+	
+	if(memcmp(read_buffer,wav_header, sizeof(read_buffer)))
+		return -1;
+		
+	return 0;
+}
+
+void wav_rewind(FIL * file) {
+	f_lseek(file, sizeof(wav_header));
+}	
+
 int wav_create_header(FIL * file) {
 	unsigned int ret;
 	
