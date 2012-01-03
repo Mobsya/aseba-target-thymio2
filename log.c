0fecadc67375dc1c5e02a433e6b1950644882338
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2012-01-03 09:15:53 +0000

    Move log pages to before settings page

--- log.c
+++ log.c
@@ -325,7 +325,7 @@ void AsebaVMRunCB(AsebaVMState *vm) {
 
 // Flash write/read part
 
-#define LOG_FLASH_ADDRESS (FLASH_END - 0xc00 - 0x400 - 0x800)
+#define LOG_FLASH_ADDRESS (FLASH_END - 0x1000 - 0x400 - 0x800)
 unsigned char log_flash[INSTRUCTIONS_PER_PAGE * 2*2] __attribute__ ((space(prog), section(".log_flash"), noload, address(LOG_FLASH_ADDRESS)));
 
 #define PAGE_0 LOG_FLASH_ADDRESS
