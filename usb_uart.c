b49e80cd513f1e8a065c12ea28ad8da71ac25472
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2012-11-06 12:37:59 +0100

    Fix license headers

--- usb_uart.c
+++ usb_uart.c
@@ -1,5 +1,27 @@
+/*
+Software License Agreement:
+
+ The software supplied herewith by Microchip Technology Incorporated
+ (the <93>Company<94>) for its PIC® Microcontroller is intended and
+ supplied to you, the Company<92>s customer, for use solely and
+ exclusively on Microchip PIC Microcontroller products. The
+ software is owned by the Company and/or its supplier, and is
+ protected under applicable copyright laws. All rights are reserved.
+ Any use in violation of the foregoing restrictions may subject the
+ user to criminal sanctions under applicable laws, as well as to
+ civil liability for the breach of the terms and conditions of this
+ license.
+
+ THIS SOFTWARE IS PROVIDED IN AN <93>AS IS<94> CONDITION. NO WARRANTIES,
+ WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+
+*/
+
 /* This file is not released under LGPLv3 as some Microchip code is used.
- * See license.txt for more details
  */
 
 
